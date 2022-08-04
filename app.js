01db04f5d4d361bb31ae3240f3b3d335e32e5b96
Nguyen Tan Duy <duy.t.nguyen@taptap.com.vn>
2022-08-04 15:45:15 +0700

    change api url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const messageDisplay = document.querySelector(".message-container");
 let wordle;
 
 const getWordle = () => {
-  fetch("http://wordle-server-duy.herokuapp.com/word")
+  fetch("https://wordle-server-duy.herokuapp.com/word")
     .then((response) => response.json())
     .then((data) => {
       wordle = data[0];
